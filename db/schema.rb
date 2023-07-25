da92895b6116b2176773183db401b698b3ac4b28
Ritika Rawat <Ritikarawat220@gmail.com>
2023-07-25 17:43:45 +0530

    Add test for model entity and update migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_25_082623) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_25_121031) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,6 +34,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_25_082623) do
     t.decimal "amount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "author_id", null: false
+    t.index ["author_id"], name: "index_entities_on_author_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -54,4 +56,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_25_082623) do
   end
 
   add_foreign_key "categories", "users", column: "author_id"
+  add_foreign_key "entities", "users", column: "author_id"
 end
